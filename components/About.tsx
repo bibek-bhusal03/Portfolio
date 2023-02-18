8784c83d268cff95f63926cd5dcff178d7984073
Bibek <bibekbhusal455@gmail.com>
2023-02-18 09:07:11 +0545

    third commit

--- components/About.tsx
+++ components/About.tsx
@@ -28,7 +28,7 @@ export default function About({}: Props) {
       transition={{
         duration: 1.2,
       }}
-      src='./profile.jpg'
+      src='./profile.JPG'
       className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[450px] xl:mt-10'
       />
       <div className='space-between px-0 md:px-10'>
